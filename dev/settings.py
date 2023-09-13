559ab0eb9dc29e2b06afc4443c637ae283eb1468
Zazastrus <al.schz5@outlook.com>
2023-09-12 18:50:42 -0600

    Debug OFF

--- dev/settings.py
+++ dev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-54j5*zdi=d!vblm7=%x0ste_7qrhj01916*h7=%h%crj$5@=$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
